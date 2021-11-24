54dcff9abffd0947b4368d84df8381f0298ed8e8
Caspian Maclean <caspianmaclean@gmail.com>
2021-11-24 19:30:25 +0800

    make a table of users

--- src/Server.java
+++ src/Server.java
@@ -1,47 +1,21 @@
-/*
- * Copyright (c) 2004, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met:
- *
- * -Redistributions of source code must retain the above copyright
- *  notice, this list of conditions and the following disclaimer.
- *
- * -Redistribution in binary form must reproduce the above copyright
- *  notice, this list of conditions and the following disclaimer in
- *  the documentation and/or other materials provided with the
- *  distribution.
- *
- * Neither the name of Oracle nor the names of
- * contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.
- *
- * This software is provided "AS IS," without a warranty of any
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
- * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
- * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
- * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
- * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,
- * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
- * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *
- * You acknowledge that Software is not designed, licensed or
- * intended for use in the design, construction, operation or
- * maintenance of any nuclear facility.
- */
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
+/**
+ *
+ * @author 30039802 - Caspian Maclean
+ */
 public class Server implements Hello, Session {
+
+    UserTable users = new UserTable();
+
+    public Server() {
+        users.addUserToTable("fred", "ffff");
+        users.addUserToTable("admin", "admin");
+    }
 
     private void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
@@ -52,19 +26,17 @@ public class Server implements Hello, Session {
         this.admin = admin && loggedIn;
     }
 
-    public Server() {}
-
-	@Override
+    @Override
     public String sayHello() {
         return "Hello, world!";
     }
-    
+
     private boolean loggedIn = false;
     private boolean admin = false;
     // admin is always false when loggedIn == false. setAdmin and setLoggedIn
     // maintain this property.
     private String sessionUsername = "";
-        
+
     // Session methods
     @Override
     public boolean login(String username, String password) {
@@ -81,16 +53,16 @@ public class Server implements Hello, Session {
         }
         return loggedIn;
     }
-    
+
     @Override
     public boolean isLoggedIn() {
         return loggedIn;
     }
-    
+
     @Override
     public boolean isAdmin() {
         return admin;
-    }  
+    }
     @Override
     public String getMessage() {
         if(!loggedIn) {
@@ -100,12 +72,12 @@ public class Server implements Hello, Session {
         } else {
             return "Hello" + sessionUsername + ". Welcome to the server.";
         }
-    } 
+    }
     @Override
     public String createLogin(String username, String password) {
         return "failed, not supported yet";
     }
-    
+
     public static void main(String args[]) {
 
         try {
---
+++ src/UserTable.java
@@ -0,0 +1,38 @@
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+/**
+ *
+ * @author 30039802 - Caspian Maclean
+ */
+public class UserTable {
+
+    private HashMap<String, User> usersByName = new HashMap<>();
+
+    public boolean passwordCorrect(String user, String passwordIn) {
+        User foundUser = usersByName.get(user);
+        // null if not found, which needs to be checked for
+        if (foundUser != null) {
+            //return passwordIn.equals(storedPassword);
+            try {
+                return PasswordUtilities.isCorrectPassword(foundUser, passwordIn);
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+                // in case of failure, don't allow the login
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void addUserToTable(String userName, String password) {
+        try {
+            User user = PasswordUtilities.prepareUser(userName, password);
+            usersByName.put(userName, user);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            // Shouldn't fail, but if it does, don't add the user
+            System.out.println("Unexpected exception - cannot add user");
+        }
+    }
+}
